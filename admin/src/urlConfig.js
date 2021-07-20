5dac80222269a8d5a89f93078b27aa4efe974db4
BabluKD <bablu2vaibhaw@gmail.com>
2021-07-20 12:48:15 +0530

    urlConfigg changed

--- admin/src/urlConfig.js
+++ admin/src/urlConfig.js
@@ -1,4 +1,4 @@
-const baseUrl = process.env.API || "http://localhost:5000";
+const baseUrl = process.env.API || "https://admin-pronto.herokuapp.com";
 //const baseUrl = "http://localhost:2000";
 
 export const api = `${baseUrl}/api`;
